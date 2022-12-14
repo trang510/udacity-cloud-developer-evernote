6efdbd89192231aae8e087d5090b4f98e80301ae
trangnd <trangnd@vng.com.vn>
2022-12-15 05:32:43 +0700

    change frontend version

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ docker build -t api-user ./api-user
 docker build -t frontend ./frontend
 docker tag api-feed trangnd/api-feed:v1
 docker tag api-user trangnd/api-user:v1
-docker tag frontend trangnd/frontend:v1
+docker tag frontend trangnd/frontend:v2
 docker push trangnd/api-feed:v1
 docker push trangnd/api-user:v1
-docker push trangnd/frontend:v1
+docker push trangnd/frontend:v2
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a766571bcda184878be12c148ef857a0-1302097198.us-east-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
