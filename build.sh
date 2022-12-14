ed19c2736a4df57645e75a86d220b52ff81ae053
trangnd <trangnd@vng.com.vn>
2022-12-14 21:13:31 +0700

    init app

---
+++ build.sh
@@ -0,0 +1,10 @@
+docker --version # print the version for logging
+docker build -t api-feed ./api-feed
+docker build -t api-user ./api-user
+docker build -t frontend ./frontend
+docker tag api-feed trangnd/api-feed:v1
+docker tag api-user trangnd/api-user:v1
+docker tag frontend trangnd/frontend:v1
+docker push trangnd/api-feed:v1
+docker push trangnd/api-user:v1
+docker push trangnd/frontend:v1
